554134d4497f0431e84d9befc711bcf14baf4d12
Brenno Marks <marksbre@gmail.com>
2016-12-06 22:42:00 -0200

    Loading the path /lib automatically.

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,12 @@ module Houzel
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+   
+    # Custom directories with classes and modules you want to be autoloadable.
+    config.autoload_once_paths += %W{#{config.root}/lib}
   end
 end
