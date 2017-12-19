2dad4c1d79c9ffd2f028f93fb4a3a97fadccc762
Brenno Marks <marksbre@gmail.com>
2017-12-19 21:57:00 -0200

    Going over sprockets. Using webpacker as assets pipeline.

--- app/assets/packs/application.js
+++ app/assets/packs/application.js
@@ -7,6 +7,11 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+/* Stylesheet */
+import 'bootstrap';
+import '../stylesheets/application.less'
+
+/* Application */
 export {default as Houzel} from '../javascripts/backbone/app.js.coffee';
 
 document.addEventListener("DOMContentLoaded", function() {
