97929eb347851c7d5358e45e75736484164fdf86
Brenno Marks <marksbre@gmail.com>
2018-02-05 20:15:00 -0200

    Set locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+  before_action :gon_push_appdata
+
+  private
+
+  def set_locale
+    locale = http_accept_language
+              .language_region_compatible_from I18n.available_locales.map(& :to_s )
+
+    locale ||= DEFAULT_LANGUAGE
+    I18n.locale = locale
+  end
+
+  def gon_push_appdata
+    gon.push(locale: I18n.locale)
+  end
+
 end
