b61029f41fc5b0fd7d3818f0d7b19675a096c6ab
Brenno Marks <marksbre@gmail.com>
2018-02-13 22:55:00 -0200

    Add 'connect-src' directive to headers

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,6 +1,7 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
     default_src: %w('none'),
+    connect_src: %w('self'),
     font_src: %w('self' data: fonts.gstatic.com),
     img_src:  %w['self' data: blob: *],
     script_src: %w['self' 'unsafe-inline'] ,
