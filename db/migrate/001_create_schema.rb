5efb63d119f08466a48dc6a3108813cafd6f4c0c
Brenno Marks <marksbre@gmail.com>
2019-05-17 19:32:00 -0300

    Update table schema 'patient'

--- db/migrate/001_create_schema.rb
+++ db/migrate/001_create_schema.rb
@@ -5,27 +5,31 @@ Sequel.migration do
       primary_key :id
       column :guid,           String,     :null=>false
       column :closed_account, FalseClass, :default=>false
+      column :owner_id,       Integer
       column :start_at,       DateTime
       column :created_at,     DateTime,   :null=>false
       column :updated_at,     DateTime,   :null=>false
     end
 
     add_index :person, [:guid], :name => 'index_person_on_guid', :using => :btree, :unique=>true
+    add_index :person, [:owner_id], :name => 'index_person_on_user_id'
 
     create_table :patient do
       primary_key :id
-      column :name, String, :null=>false, :size=>255
-      column :birthday, Date
-      column :sex, String, :size=>1
-      column :profession, String, :size=>127
-      column :address, String, :size=>382
-      column :location, String, :size=>127
-      column :zip_code, String, :size=>50
-      column :phone, String, :size=>63
-      column :email, String, :size=>127
-      column :parent, String, :size=>255
-      column :blood_type, String, :size=>5
-      column :observation, String
+      column :name,            String,   :null=>false, :size=>255
+      column :birthday,        Date
+      column :sex,             String,   :size=>63
+      column :profession,      String,   :size=>127
+      column :site,            String,   :size=>255
+      column :address,         String,   :size=>382
+      column :zip_code,        String,   :size=>63
+      column :phone,           String,   :size=>63
+      column :email,           String,   :size=>127
+      column :parent_name,     String,   :size=>255
+      column :blood_type,      String,   :size=>32
+
+      column :observation,     String
+      column :removed_at,      DateTime
       column :created_at,  DateTime, :null=>false
       column :updated_at,  DateTime, :null=>false
     end
