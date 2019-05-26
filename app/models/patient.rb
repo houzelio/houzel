c609a465335a0fe0207a4babad6ccf26988bb02d
Brenno Marks <marksbre@gmail.com>
2019-05-26 17:49:00 -0300

    Validate the model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,15 @@
 class Patient < Sequel::Model
   plugin :timestamps, force: true, update_on_create: true
 
+  def validate
+    super
+    validates_max_length 255, :name
+    validates_max_length 63,  :phone
+    validates_max_length 127, :email
+    validates_max_length 255, :site
+    validates_max_length 63,  :zip_code
+    validates_max_length 255, :address
+    validates_max_length 127, :profession
+    validates_max_length 255, :parent_name
+  end
 end
