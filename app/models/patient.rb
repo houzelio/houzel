14c66329df88155a5094759ec33dea5aaa750b53
Brenno Marks <marksbre@gmail.com>
2019-05-26 17:34:00 -0300

    Force a timestamp on create and update

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,4 @@
 class Patient < Sequel::Model
+  plugin :timestamps, force: true, update_on_create: true
 
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 class Person < Sequel::Model
   include Houzel::Guid
 
+  plugin :timestamps, force: true, update_on_create: true
+
   def to_param
     self.guid
   end
