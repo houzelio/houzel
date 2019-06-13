3ddfa2a7c10ce7af5cece41ffb82fe22b68f3415
Brenno Marks <marksbre@gmail.com>
2019-06-13 20:13:00 -0300

    Add routes for access control

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Houzel::Application.routes.draw do
   root to: "application#index"
 
+  if Sequel::Model.db.table_exists?(:user)
+    devise_for :user, controllers: { registrations: "registration", sessions: "session" },
+      skip: [:registration, :session]
+  end
+
+  devise_scope :user do
+    get    "signin",  to: "session#new",         as: :new_user_session
+    post   "signin",  to: "session#create",      as: :user_session
+    delete "signout", to: "session#destroy",     as: :destroy_user_session
+
+    get    "signup",  to: "registration#new",    as: :new_user_registration
+    post   "signup",  to: "registration#create", as: :user_registration
+  end
+
+  resources :user, only: :destroy
   resources :patient
   resources :calendar, :except => [:index]
 end
