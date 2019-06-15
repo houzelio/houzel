ad4ab20b5371401304c3b2446fc690cadf2f2d36
Brenno Marks <marksbre@gmail.com>
2019-06-15 20:23:00 -0300

    Add relations to person

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,11 @@ class Person < Sequel::Model
 
   plugin :timestamps, force: true, update_on_create: true
 
+  many_to_one :user, key: :owner_id
+  one_to_one  :profile
+
+  delegate :name, :to => :profile
+
   def to_param
     self.guid
   end
