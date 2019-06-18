b68b453cad00eab24c04dbadf6bea155514ea02b
Brenno Marks <marksbre@gmail.com>
2019-06-18 20:07:00 -0300

    Add the role model

---
+++ app/models/role.rb
@@ -0,0 +1,20 @@
+class Role < Sequel::Model
+  plugin :timestamps, force: true, update_on_create: true
+  plugin :def_dataset_method
+
+  many_to_one :person
+
+  subset(:admins, name: "admin")
+
+  def self.is_admin?(person)
+    first(person: person, name: ["admin", "owner"]).present?
+  end
+
+  def self.add_admin(person)
+    find_or_create(person: person, name: "admin")
+  end
+
+  def self.has_owner?
+    first(name: "owner").present?
+  end
+end
