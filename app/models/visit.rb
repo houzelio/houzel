b1f9dc09dc70166dc73a663b1d1aeba8112b5f3f
Brenno Marks <marksbre@gmail.com>
2019-06-21 22:09:00 -0300

    Add appointment to visit

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,5 +1,10 @@
 class Visit < Sequel::Model
   plugin :timestamps, force: true, update_on_create: true
+  plugin :delay_set_association
+  plugin :nested_association
 
   many_to_one :patient
+  one_to_one  :appointment
+
+  nested_association :appointment
 end
