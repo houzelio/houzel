db66d0a14a959797c6b19b5d884e345584e6dc3d
Brenno Marks <marksbre@gmail.com>
2019-07-09 19:18:00 -0300

    Validate appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,10 @@
 class Appointment < Sequel::Model
   plugin :timestamps, force: true, update_on_create: true
   many_to_one :visit
+
+  def validate
+    super
+    validates_presence :start_date
+    validates_presence :end_date
+  end
 end
