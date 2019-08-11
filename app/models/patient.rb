c56081362270816b5d2b62a7c36863adb3508f97
Brenno Marks <marksbre@gmail.com>
2019-08-11 19:29:00 -0300

    Fix missing relations in patient

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,10 @@
 class Patient < Sequel::Model
   plugin :timestamps, force: true, update_on_create: true
+  plugin :many_through_many
+
+  one_to_many :visit
+  one_to_many :invoice
+  many_through_many :appointment, [[:visit, :patient_id, :id]], right_primary_key: :visit_id
 
   def validate
     super
