70f30283d4b87272dc2249d41e0e971581480a57
Brenno Marks <marksbre@gmail.com>
2019-08-17 15:45:00 -0300

    Update associations

--- app/models/invoice_service.rb
+++ app/models/invoice_service.rb
@@ -2,4 +2,5 @@ class InvoiceService < Sequel::Model
   plugin :timestamps, force: true, update_on_create: true
 
   many_to_one :service, key: :reference_id
+  many_to_one :invoice
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,5 @@
 class Service < Sequel::Model
   plugin :timestamps, force: true, update_on_create: true
+
+  one_to_many :invoice_service
 end
