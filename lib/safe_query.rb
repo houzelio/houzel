c0b9b4b568d04b3aff5d5d45e43927f80fd5f2a1
Brenno Marks <marksbre@gmail.com>
2019-09-23 18:18:00 -0300

    Add the query class 'MergedInvoiceService'

--- lib/safe_query.rb
+++ lib/safe_query.rb
@@ -7,6 +7,20 @@ module SafeQuery
     end
   end
 
+  class MergedInvoiceService < Base
+    def initialize(invoice)
+      @invoice = invoice
+    end
+
+    def services
+      r = InvoiceService.select_all(:invoice_service)
+      r = r.left_join(:service, id: :reference_id)
+      r = r.where(invoice: @invoice)
+      r = r.select_append(sel_column(:service, :value, :service_value))
+      r
+    end
+  end
+
   class MedProfessionals < Base
     def examiners
       r = Person.select(sel_column(:person, :id), sel_column(:profile, :name))
