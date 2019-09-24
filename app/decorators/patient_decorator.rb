b5b41244d2bc9d317f8da9486730a59e8d4ba920
Brenno Marks <marksbre@gmail.com>
2019-09-24 19:26:00 -0300

    Return the date formatted by locale

--- app/decorators/patient_decorator.rb
+++ app/decorators/patient_decorator.rb
@@ -1,6 +1,10 @@
 class PatientDecorator < ApplicationDecorator
   delegate_all
 
+  def birthday
+    I18n.l(patient.birthday) if patient.birthday
+  end
+
   def appointments
     if context[:relation].eql? "appointment"
       patient.appointment || {}
