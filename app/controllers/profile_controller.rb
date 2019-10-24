8b936b3b67098a789f109adc82febb4e0ae4197c
Brenno Marks <marksbre@gmail.com>
2019-10-24 20:48:00 -0300

    Update profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -7,6 +7,17 @@ class ProfileController < ApplicationController
     @profile = ProfileDecorator.new(current_user.person.profile)
   end
 
+  def update
+    changed_language = current_user.update_fields({ language: params[:locale] }, [:language])
+
+    profile = Profile.first(id: params[:id])
+    if profile.update_fields(profile_params, profile_fields) || changed_language
+      render json: { path: profile_index_path }, :status => :ok
+    else
+      respond_with profile
+    end
+  end
+
   private
 
   def profile_params
