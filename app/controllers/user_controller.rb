91e99e3646a5fc007f7249e45f994abdf33d4346
Brenno Marks <marksbre@gmail.com>
2019-10-24 21:51:00 -0300

    Allow to update email and/or password

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,16 @@
 class UserController < ApplicationController
   respond_to :json
 
+  def update
+    @user = current_user
+
+    if params[:password_params]
+      change_password(user_params)
+    else params[:email]
+      change_email(user_params)
+    end
+  end
+
   def destroy
     if !params.dig(:user,:current_password) && current_user.admin?
       user = User.first(id: params[:id])
@@ -11,6 +21,32 @@ class UserController < ApplicationController
       if !params.dig(:user,:current_password) && !current_user.admin?
         respond_with_message(I18n.t('general.messages.permission_denied'), "error", 401)
       end
+    end
+  end
+
+  private
+
+  def change_password(user_data)
+    if @user.update_with_password(user_data)
+      flash[:success] = I18n.t('user.messages.password_changed')
+    else
+      respond_with_message(I18n.t('user.messages.password_not_changed'), "error", 422)
+    end
+  end
+
+  def change_email(user_data)
+    if @user.update_fields(user_data, [:email])
+      respond_with_message(I18n.t('user.messages.settings_updated'), "success", :ok)
+    else
+      respond_with @user
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email).tap do |p|
+      p[:current_password] = params.dig(:password_params, :current_password)
+      p[:password] = params.dig(:password_params, :new_password)
+      p[:password_confirmation] = params.dig(:password_params, :password_confirmation)
     end
   end
 end
