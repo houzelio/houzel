a01d3909b036f456816818835df13bf427cf4494
Brenno Marks <marksbre@gmail.com>
2019-10-26 13:55:00 -0300

    Respond with html and json formats

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,6 +2,10 @@ class SessionController < Devise::SessionsController
 
   def destroy
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-    redirect_to after_sign_out_path_for(resource_name)
+
+    respond_to do |format|
+      format.html { redirect_to after_sign_out_path_for(resource_name) }
+      format.json { render json: {}, :status => 401}
+    end
   end
 end
