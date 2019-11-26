ac23c46c556d194b7a4fce69b7434715e5be3e4a
Brenno Marks <marksbre@gmail.com>
2019-11-26 20:25:00 -0200

    Do not update patient on visit

--- app/controllers/visit_controller.rb
+++ app/controllers/visit_controller.rb
@@ -48,7 +48,7 @@ class VisitController < ApplicationController
     visit.set_medical_history_fields(mcl_history_params, mcl_history_fields)
     checked_out = visit_params[:end_date].present? && !visit.end_date
 
-    if visit.update_fields(visit_params, visit_fields)
+    if visit.update_fields(visit_params, visit_fields.without(:patient_id))
       respond_with_message(I18n.t(checked_out ? 'visit.messages.checked_out' : 'visit.messages.updated'), "success", :ok)
     else
       respond_with visit
