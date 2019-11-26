aefc86bc1509db259e4388192f9b0d04d85273d9
Brenno Marks <marksbre@gmail.com>
2019-11-26 19:32:00 -0200

    Decorate collection when on scope 'extended'

--- app/decorators/patient_decorator.rb
+++ app/decorators/patient_decorator.rb
@@ -5,9 +5,7 @@ class PatientDecorator < ApplicationDecorator
     I18n.l(patient.birthday) if patient.birthday
   end
 
-  def appointments
-    if context[:relation].eql? "appointment"
-      patient.appointment || {}
-    end
+  def scheduled_appointments
+    AppointmentDecorator.decorate_collection(patient.scheduled_appointments) if context[:scope].eql? "extended"
   end
 end
