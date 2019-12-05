0061fc4596ef51d6345178a43840912317133187
Brenno Marks <marksbre@gmail.com>
2019-12-05 18:45:00 -0200

    Config the manifest on test environment

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,6 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
 const environment = require('./environment')
+const manifestPlugin = environment.plugins.get('Manifest')
+
+manifestPlugin.options.writeToFileEmit = process.env.NODE_ENV !== 'test'
 
 module.exports = environment.toWebpackConfig()
