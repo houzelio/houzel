dd2b5b77961ada71a57e7aebffedf75d490f6412
Brenno Marks <marksbre@gmail.com>
2020-12-02 18:57:00 -0300

    Disable Recoverable in the beta version

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < Sequel::Model
   plugin :timestamps, force: true, update_on_create: true
   plugin :devise
   plugin :delay_set_association
-  devise :database_authenticatable, :registerable, :recoverable,
-         :lockable, :lock_strategy => :none, :unlock_strategy => :none
+  devise :database_authenticatable, :registerable, :lockable,
+         :lock_strategy => :none, :unlock_strategy => :none
 
   one_to_one :person, key: :owner_id
 
